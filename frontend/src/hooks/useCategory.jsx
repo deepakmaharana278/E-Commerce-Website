9bee7f7d40e5b8d15efb7eb32d1097f902e39886
Jitendra Maharana <deepakmaharana3500@gmail.com>
2025-10-01 21:30:18 +0530

    for deployment

--- frontend/src/hooks/useCategory.jsx
+++ frontend/src/hooks/useCategory.jsx
@@ -1,22 +1,27 @@
-import axios from 'axios'
-import React,{useState,useEffect} from 'react'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 export default function useCategory() {
-    const [categories, setCategories] = useState([])
-    
-    // get category
-    const getCategories = async () => {
-        try {
-            const { data } = await axios.get('/api/v1/category/get-category')
-            setCategories(data?.category)
-        } catch (error) {
-            console.log(error);
-        }
+  const [categories, setCategories] = useState([]);
+
+  // create Axios instance with base URL from env
+  const API = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+  });
+
+  // get category
+  const getCategories = async () => {
+    try {
+      const { data } = await API.get("/api/v1/category/get-category");
+      setCategories(data?.category);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    useEffect(() => {
-        getCategories();
-    }, []);
+  useEffect(() => {
+    getCategories();
+  }, []);
 
-    return categories;
+  return categories;
 }
